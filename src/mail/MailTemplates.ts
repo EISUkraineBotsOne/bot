cbd743ad20b25a367dcdcb2d8dde050d541270c5
Artem Artemenko <artmtn@gmail.com>
2017-02-08 01:10:31 +0200

    Added email-template (ejs) and created base templates structure

--- src/mail/MailTemplates.ts
+++ src/mail/MailTemplates.ts
@@ -1,4 +1,4 @@
-/* Predefined e-mail templates */
+/* Interfaces for predefined e-mail templates */
 
 export interface CustomerNotification {
     customerName: string,
@@ -28,55 +28,10 @@ export interface ErrorNotification {
 
 export function createTestHelloMsg() {
     return {
-        from: 'eisbot.hackathon@gmail.com',
+        from: process.env.MAIL_LOGIN,
         to: 'eisbot.hackathon@gmail.com',
         subject: '👻 Hack-a-thon 👻',
         text: 'Let\'s code something useful',
         html: '<b><i>Hello from EIS Bot</i></b>'
-    }
-}
-
-export function createCustomerNotification(notification: CustomerNotification) {
-    return {
-        from: process.env.MAIL_LOGIN,
-        to: 'eisbot.hackathon@gmail.com',
-        subject: 'Customer notification',
-        text: `Customer ${notification.customerName} asked to notify you about an accident for policy #${notification.policyNumber}.
-Accident data:
-Latitude: ${notification.coordinates.latitude}, Longitude: ${notification.coordinates.longitude} [http://maps.google.com/?q=${notification.coordinates.latitude},${notification.coordinates.longitude}]
-Date: ${notification.date}`,
-        html: `Customer <b>${notification.customerName}</b> asked to notify you about an accident for policy <b>#${notification.policyNumber}</b>.
-<h4>Accident data:</h4>
-<b>Latitude</b>: ${notification.coordinates.latitude}, <b>Longitude</b>: ${notification.coordinates.longitude}<br/>
-<a href="http://maps.google.com/?q=${notification.coordinates.latitude},${notification.coordinates.longitude}" target="_blank">View accident location</a><br/><br/>
-<b>Date</b>: ${notification.date}`
-    };
-}
-
-export function createClaimNotification(notification: ClaimCreatedNotification) {
-    return {
-        from: process.env.MAIL_LOGIN,
-        to: 'eisbot.hackathon@gmail.com',
-        subject: 'Claim created',
-        text: `Claim #${notification.claimNumber} for policy #${notification.policyNumber} has been initiated and opened by customer ${notification.customerName} request.`,
-        html: `Claim <b>#${notification.claimNumber}</b> for policy <b>#${notification.policyNumber}</b> has been initiated and opened by customer <b>${notification.customerName}</b> request.`
-    }
-}
-
-export function createErrorNotification(notification: ErrorNotification) {
-    return {
-        from: process.env.MAIL_LOGIN,
-        to: 'eisbot.hackathon@gmail.com',
-        subject: 'System error occurred during conversation with customer',
-        text: `Claim for customer #${notification.customerNumber} was not created due to system error. Customer policy #${notification.policyNumber}.
-Accident data:
-Latitude: ${notification.coordinates.latitude}, Longitude: ${notification.coordinates.longitude} [http://maps.google.com/?q=${notification.coordinates.latitude},${notification.coordinates.longitude}]
-Date: ${notification.date}`,
-        html: `Claim for customer <b>#${notification.customerNumber}</b> was not created due to system error.<br/>
-Customer policy <b>#${notification.policyNumber}</b>.
-<h4>Accident data:</h4>
-<b>Latitude</b>: ${notification.coordinates.latitude}, <b>Longitude</b>: ${notification.coordinates.longitude}<br/>
-<a href="http://maps.google.com/?q=${notification.coordinates.latitude},${notification.coordinates.longitude}" target="_blank">View accident location</a><br/><br/>
-<b>Date</b>: ${notification.date}`
     }
 }
--- src/mail/Mailer.ts
+++ src/mail/Mailer.ts
@@ -1,4 +1,7 @@
 /* Service for sending e-mails via gmail */
+let EmailTemplate = require('email-templates').EmailTemplate;
+let path = require('path');
+
 import * as mt from "./MailTemplates"
 
 const nodemailer = require('nodemailer');
@@ -31,13 +34,67 @@ export function sendMail(mailParams: MailOptions) {
 }
 
 export function sendCustomerNotification(data: mt.CustomerNotification) {
-    sendMail(mt.createCustomerNotification(data));
+    let template = new EmailTemplate(path.join(__dirname, 'templates', 'customer-notification'));
+    template.render({data}, function(err, results : {text?:string, html?:string}) {
+        if (err) {
+            console.error(err);
+        }
+
+        transport.sendMail({
+            from: process.env.MAIL_LOGIN,
+            to: 'eisbot.hackathon@gmail.com',
+            subject: 'Customer notification',
+            text: results.text,
+            html: results.html
+        }, function (error, info) {
+            if (error) {
+                console.error(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        })
+    })
 }
 
 export function sendClaimCreated(data : mt.ClaimCreatedNotification) {
-    sendMail(mt.createClaimNotification(data));
+    let template = new EmailTemplate(path.join(__dirname, 'templates', 'claim-notification'));
+    template.render({data}, function (err, results: {text?:string, html?:string}) {
+        if(err) {
+            console.error(err);
+        }
+
+        transport.sendMail({
+            from: process.env.MAIL_LOGIN,
+            to: 'eisbot.hackathon@gmail.com',
+            subject: 'Claim created',
+            text: results.text,
+            html: results.html
+        }, function (error, info) {
+            if(error) {
+                console.error(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        })
+    })
 }
 
 export function sendError(data : mt.ErrorNotification) {
-    sendMail(mt.createErrorNotification(data));
+    let template = new EmailTemplate(path.join(__dirname, 'templates', 'error-notification'));
+    template.render({data}, function(err, results: {text?:string, html?:string}) {
+        if(err) {
+            console.error(err);
+        }
+
+        transport.sendMail({
+            from: process.env.MAIL_LOGIN,
+            to: 'eisbot.hackathon@gmail.com',
+            subject: 'System error occurred during conversation with customer',
+            text: results.text,
+            html: results.html
+        }, function (error, info) {
+            if(error) {
+                console.error(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        })
+    })
 }
